ad1c8ebdf5f932a219e922ff7a73f30f048c06fd
SanjaySK25 <sanjay12563at@gmail.com>
2023-03-27 23:05:38 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express') // importing express
 const app = express() // getting the app portion from the express package we imported
 const expressLayouts = require('express-ejs-layouts') // importing express layout package
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine','ejs') // setting the view engine to ejs
 app.set('views', __dirname + '/views') // setting the location of views
 app.set('layout', 'layouts/layout') // setting the base layout
 app.use(expressLayouts) // setting to use the express layouts
 app.use(express.static('public')) // telling where our public files are
+app.use(bodyParser.urlencoded({ limit:'10mb', extended:false }));
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
@@ -22,5 +25,6 @@ db.on('error', () => console.log('error'));
 db.once('open', () => console.log("Connected to Mongoose!"))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000) // Basically connects you to a port
